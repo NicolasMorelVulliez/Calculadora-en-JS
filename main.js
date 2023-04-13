578a84469cb76785c75fe804080e0705982c8987
Nicolas Morel Vulliez <nicomorel2002@gmail.com>
2023-04-13 15:21:03 -0300

    agregue un do/while para saber si el usuario queria seguir usando la calculadora

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
-let operacion = prompt("¿Qué operación desea realizar? (suma, resta, multiplicacion, division)");
 
-let num1 = parseFloat(prompt("Ingrese el primer número:"));
-let num2 = parseFloat(prompt("Ingrese el segundo número:"));
+do {
+    let operacion = prompt("¿Qué operación desea realizar? (suma, resta, multiplicacion, division)");
 
-let resultado;
+    if (operacion === "suma" || operacion === "resta" || operacion === "multiplicacion" || operacion === "division") {
 
-switch (operacion) {
-    case "suma":
-        resultado = num1 + num2;
-        break;
-    case "resta":
-        resultado = num1 - num2;
-        break;
-    case "multiplicacion":
-        resultado = num1 * num2;
-        break;
-    case "division":
-        resultado = num1 / num2;
-        break;
-    default:
+        let num1 = parseFloat(prompt("Ingrese el primer número:"));
+        let num2 = parseFloat(prompt("Ingrese el segundo número:"));
+        let resultado;
+        switch (operacion) {
+            case "suma":
+                resultado = num1 + num2;
+                break;
+            case "resta":
+                resultado = num1 - num2;
+                break;
+            case "multiplicacion":
+                resultado = num1 * num2;
+                break;
+            case "division":
+                resultado = num1 / num2;
+                break;
+        }
+        alert("El resultado de la operación " + operacion + " es: " + resultado);
+    }
+    else {
         alert("Operación no válida");
-}
+    }
+} while (confirm("¿Deseas realizar otra operación?"));
 
-alert("El resultado de la operación " + operacion + " es: " + resultado);
+
+
+
+
+
+
